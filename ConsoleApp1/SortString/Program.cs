7605f2e2646ca835db9e97f96bca71985064026e
Ambrus Bence <o.g.bence@gmail.com>
2018-07-03 18:45:22 +0200

    task5

---
+++ ConsoleApp1/SortString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+            Console.WriteLine("----------------------------");
+            Array.Sort(words);
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+            Console.WriteLine("----------------------------");
+            string s = string.Join(" ", words);
+            Console.WriteLine(s);
+
+        }
+    }
+}
